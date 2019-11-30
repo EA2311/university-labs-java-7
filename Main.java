bfcd6f6f0b09ad41cea8c324ca6dada870916b62
EA2311 <54818097+EA2311@users.noreply.github.com>
2019-11-30 19:15:22 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,23 +1,8 @@
 package onpu;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
 public class Main {
 
-    public static void main(String s[]) {
-        final JFrame f = new JFrame("Paint");
-        f.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                f.dispose();
-            }
-        });
-        f.setBounds(0,0, 1000, 1000);
-        JOptionPane.showMessageDialog((Component) null, "Welcome!\nClick anywhere to start drawing.\nUse only arrow keys.\nPress SPACE to clear field.");
-        final Canvas c = new KeyDraw();
-        f.add(c);
-        f.setVisible(true);
+    public static void main(String[] args) {
+	Drawing draw = new Drawing();
     }
 }
